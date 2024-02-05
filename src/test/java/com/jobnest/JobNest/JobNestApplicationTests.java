ad73c4331e262753707852d976b8cd1564220bd1
Ganne Supriya <gannesupriya@gmail.com>
2024-02-05 19:19:48 +0530

    commit

---
+++ src/test/java/com/jobnest/JobNest/JobNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobnest.JobNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
